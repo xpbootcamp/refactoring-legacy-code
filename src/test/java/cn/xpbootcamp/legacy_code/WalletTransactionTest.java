f9029d5208e2d75fd2d7d2cbc2550bc272701a14
Yuan Shenjian <sjyuan@thoughtworks.com>
2020-03-11 11:51:24 +0800

    I.03 - Test doing nothing when executed is executed
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.Test;
 
 import javax.transaction.InvalidTransactionException;
 
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -56,6 +59,33 @@ class WalletTransactionTest {
 
         assertTrue(spyWalletTransaction.isStatusExpired());
         verify(redisDistributedLock, times(1)).lock(updatedPreAssignedId);
+    }
+
+    @Test
+    void should_do_nothing_given_transaction_is_executed() throws InvalidTransactionException {
+        long buyerId = 1L;
+        long sellerId = 2L;
+        double amount = 500.0;
+
+        WalletTransaction walletTransaction = new WalletTransaction("sjyuan123", buyerId, sellerId, amount);
+        WalletService walletService = mock(WalletServiceImpl.class);
+        String updatedPreAssignedId = "t_sjyuan123";
+        when(walletService.moveMoney(updatedPreAssignedId, buyerId, sellerId, amount)).thenReturn("123");
+
+
+        RedisDistributedLock redisDistributedLock = mock(RedisDistributedLock.class);
+        when(redisDistributedLock.lock(updatedPreAssignedId)).thenReturn(true);
+
+        walletTransaction.setWalletService(walletService);
+        walletTransaction.setRedisDistributedLock(redisDistributedLock);
+
+        walletTransaction.execute();
+
+        walletTransaction.execute();
+        assertTrue(walletTransaction.isStatusSuccessful());
+        verify(walletService, times(1)).moveMoney(updatedPreAssignedId, buyerId, sellerId, amount);
+        verify(redisDistributedLock, times(1)).lock(updatedPreAssignedId);
+
     }
 
 }
